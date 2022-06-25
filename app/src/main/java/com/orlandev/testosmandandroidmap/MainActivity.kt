d3974e0ca5fdd069356809da406d9aaa1c1c417e
Orlando N. Rodriguez <onovas05@gmail.com>
2022-06-25 11:07:15 -0400

    Add map and some functionality to test

--- app/src/main/java/com/orlandev/testosmandandroidmap/MainActivity.kt
+++ app/src/main/java/com/orlandev/testosmandandroidmap/MainActivity.kt
@@ -3,14 +3,27 @@ package com.orlandev.testosmandandroidmap
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.viewinterop.AndroidView
 import com.orlandev.testosmandandroidmap.ui.theme.TestOsmandAndroidMapTheme
+import org.osmdroid.api.IGeoPoint
+import org.osmdroid.config.Configuration
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.CustomZoomButtonsController
+import org.osmdroid.views.MapView
+import org.osmdroid.views.overlay.simplefastpoint.LabelledGeoPoint
+import org.osmdroid.views.overlay.simplefastpoint.SimpleFastPointOverlay
+import org.osmdroid.views.overlay.simplefastpoint.SimpleFastPointOverlayOptions
+import org.osmdroid.views.overlay.simplefastpoint.SimplePointTheme
+import java.io.File
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +35,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    MapScreen()
                 }
             }
         }
@@ -30,14 +43,65 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+fun MapScreen() {
+    val context = LocalContext.current
+    Configuration.getInstance().userAgentValue = BuildConfig.APPLICATION_ID
+    Configuration.getInstance().tileFileSystemCacheMaxBytes = 50L * 1024 * 1024
+    Configuration.getInstance().osmdroidTileCache =
+        File(context.cacheDir, "osmdroid").also { it.mkdir() }
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    TestOsmandAndroidMapTheme {
-        Greeting("Android")
+    val map = remember { MapView(context) }
+
+    val points = ArrayList<IGeoPoint>()
+    for (i in 0..9999) {
+        points.add(
+            LabelledGeoPoint(
+                18 + Math.random() * 5, -79 + Math.random() * 5, "Point #$i"
+            )
+        )
+    }
+
+    val pt =SimplePointTheme(points, true);
+
+// set some visual options for the overlay
+// we use here MAXIMUM_OPTIMIZATION algorithm, which works well with >100k points
+    val opt = SimpleFastPointOverlayOptions.getDefaultStyle()
+        .setAlgorithm(SimpleFastPointOverlayOptions.RenderingAlgorithm.MAXIMUM_OPTIMIZATION)
+        .setRadius(7f).setIsClickable(true).setCellSize(15)
+
+// create the overlay with the theme
+    val sfpo = SimpleFastPointOverlay(pt, opt);
+
+
+    Scaffold(
+        topBar = {
+            TopAppBar(title = { Text("Random Position") })
+        }
+    ) { paddingValues ->
+
+
+        Column(Modifier.padding(paddingValues)) {
+            Box {
+                AndroidView(
+                    factory = {
+                        map
+                    }, modifier = Modifier
+                        .fillMaxHeight()
+                        .testTag("This is a Tag")
+                ) { map ->
+                    map.zoomController.setVisibility(CustomZoomButtonsController.Visibility.SHOW_AND_FADEOUT)
+                    map.setMultiTouchControls(true)
+
+                    map.setTileSource(TileSourceFactory.DEFAULT_TILE_SOURCE)
+
+                    val mapController = map.controller
+                    mapController.setZoom(15.0)
+
+                    val startPoint = GeoPoint(20.0243223, -75.8961366)
+                    mapController.setCenter(startPoint)
+                    map.overlays.add(sfpo)
+                }
+            }
+        }
     }
 }
